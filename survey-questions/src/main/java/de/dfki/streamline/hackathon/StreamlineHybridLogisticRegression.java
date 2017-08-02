3ffa30364fe1d005bae1f77e092534305e033d23
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2017-08-02 16:21:42 +0200

    fixes random number generator and replaces sum and misclassification with error rate

--- survey-questions/src/main/java/de/dfki/streamline/hackathon/StreamlineHybridLogisticRegression.java
+++ survey-questions/src/main/java/de/dfki/streamline/hackathon/StreamlineHybridLogisticRegression.java
@@ -2,11 +2,9 @@ package de.dfki.streamline.hackathon;
 
 import de.dfki.streamline.hackathon.common.BatchPayload;
 import de.dfki.streamline.hackathon.common.StreamPayload;
-import org.apache.flink.api.common.accumulators.DoubleCounter;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.ml.common.LabeledVector;
 import org.apache.flink.ml.math.DenseVector;
@@ -19,7 +17,6 @@ import org.apache.flink.util.Collector;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author behrouz
@@ -57,32 +54,36 @@ public class StreamlineHybridLogisticRegression {
                     public void flatMap(LabeledVector training, Collector<Double> out) throws Exception {
                         LogisticRegressionModel model = pullModelFromParameterServer("hybrid-linear-regression");
 
-                        DoubleCounter sum = getRuntimeContext().getDoubleCounter("sum");
-                        DoubleCounter misClassification = getRuntimeContext().getDoubleCounter("misClassification");
 
                         Double predicted = model.predict(training.vector());
-                        if (predicted != training.label()) {
-                            misClassification.add(1.0);
-                        }
-                        sum.add(1.0);
+                        model.updateErrorRate(predicted, training.label());
 
-                        model = stochasticGradientDescentRoutine.withInitialModel(model).incrementalUpdate(training);
+                        Vector gradientVector = stochasticGradientDescentRoutine
+                                .withInitialWeights(model.getWeights())
+                                .incrementalUpdate(training)
+                                .getGradientVector();
 
-                        Double errorRate = misClassification.getLocalValue() / sum.getLocalValue();
-                        if (errorRate > USER_DEFINED_QUALITY_THRESHOLD) {
+                        Double currentErrorRate = model.getErrorRate();
+                        if (currentErrorRate > USER_DEFINED_QUALITY_THRESHOLD) {
                             Iterable<LabeledVector> historicalData = getRuntimeContext().getSideInput(sideInput);
                             List<LabeledVector> miniBatch = new LinkedList<>();
-                            Random random = new Random(42);
+                            Random random = new Random();
                             for (LabeledVector trainingVector : historicalData) {
                                 if (random.nextDouble() < MINI_BATCH_SAMPLING_RATE) {
                                     miniBatch.add(trainingVector);
                                 }
                             }
-                            model = stochasticGradientDescentRoutine.withInitialModel(model).miniBatchUpdate(miniBatch);
-                        }
 
-                        pushModelToParameterServer("hybrid-linear-regression", model);
-                        out.collect(errorRate);
+                            miniBatch.add(training);
+                            gradientVector = stochasticGradientDescentRoutine
+                                    .withInitialWeights(model.getWeights() + gradientVector)
+                                    .miniBatchUpdate(miniBatch)
+                                    .getGradientVector();
+
+                        }
+                        
+                        pushToParameterServer("hybrid-linear-regression", gradientVector, currentErrorRate);
+                        out.collect(currentErrorRate);
                     }
                 }).withSideInput(sideInput);
 
@@ -95,47 +96,16 @@ public class StreamlineHybridLogisticRegression {
         env.execute();
     }
 
-    private static void pushModelToParameterServer(String key, LogisticRegressionModel model) {
+    private static void pushToParameterServer(String key, Vector vector, Double errorRate) {
     }
 
     private static LogisticRegressionModel pullModelFromParameterServer(String key) {
         return new LogisticRegressionModel();
     }
 
-    private static void writeToKafka(DataStream<RegressionModel> model, TimeUnit minutes, int interval) {
-    }
-
     private static DataStream<String> createStreamSource(String host, int i, StreamExecutionEnvironment env) {
         return null;
     }
-
-
-    private static class StreamingSource implements org.apache.flink.streaming.api.functions.source.SourceFunction<String> {
-
-        @Override
-        public void run(SourceContext<String> sourceContext) throws Exception {
-
-        }
-
-        @Override
-        public void cancel() {
-
-        }
-    }
-
-    private static class TestStreamingSource implements org.apache.flink.streaming.api.functions.source.SourceFunction<String> {
-
-        @Override
-        public void run(SourceContext<String> sourceContext) throws Exception {
-
-        }
-
-        @Override
-        public void cancel() {
-
-        }
-    }
-
 
     private static class VectorParser implements MapFunction<String, LabeledVector> {
 
@@ -147,24 +117,23 @@ public class StreamlineHybridLogisticRegression {
 
     private static class LogisticRegressionModel {
 
-        LogisticRegressionModel withInitWeights(DenseVector zeros, double v) {
-            return this;
-        }
+        private Vector weights;
+        private Double quality;
 
-        LogisticRegressionModel withNumIterations(int i) {
-            return this;
-        }
-
-        LogisticRegressionModel setBatchDataSet(SideInput<LabeledVector> batchDataSet) {
-            return this;
-        }
-
-        DataStream<RegressionModel> prequentialTrain(DataStream<Tuple2<Integer, LabeledVector>> trainingStream) {
-            return null;
-        }
-
-        Double predict(Vector v) {
+        Double predict(org.apache.flink.ml.math.Vector v) {
             return 0.0;
+        }
+
+        public Vector getWeights() {
+            return weights;
+        }
+
+        public void updateErrorRate(Double predicted, double label) {
+
+        }
+
+        public Double getErrorRate() {
+            return quality;
         }
     }
 
@@ -184,16 +153,20 @@ public class StreamlineHybridLogisticRegression {
     }
 
     static class StochasticGradientDescent {
-        public StochasticGradientDescent withInitialModel(LogisticRegressionModel model) {
+        public StochasticGradientDescent withInitialWeights(Vector model) {
             return this;
         }
 
-        public LogisticRegressionModel incrementalUpdate(LabeledVector vector) {
+        public StochasticGradientDescent incrementalUpdate(LabeledVector vector) {
             return new LogisticRegressionModel();
         }
 
-        public LogisticRegressionModel miniBatchUpdate(List<LabeledVector> vectors) {
+        public StochasticGradientDescent miniBatchUpdate(List<LabeledVector> vectors) {
             return new LogisticRegressionModel();
+        }
+
+        Vector getGradientVector() {
+            return null;
         }
     }
 
@@ -201,8 +174,5 @@ public class StreamlineHybridLogisticRegression {
         public SideInput<LabeledVector> map(VectorParser mapFunction) {
             return null;
         }
-    }
-
-    private static class RegressionModel {
     }
 }
