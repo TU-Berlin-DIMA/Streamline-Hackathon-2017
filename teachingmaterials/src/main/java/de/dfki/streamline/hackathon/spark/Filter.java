fd278cd25d89e220ed75f3f9a6b6e7fe9cc49c68
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2017-08-07 15:04:36 +0200

    adds spark transformations and word count example

---
+++ teachingmaterials/src/main/java/de/dfki/streamline/hackathon/spark/Filter.java
@@ -0,0 +1,48 @@
+package de.dfki.streamline.hackathon.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author behrouz
+ */
+public class Filter {
+    public static void main(String[] args) throws InterruptedException {
+        // Create a local StreamingContext with two working thread and batch interval of 1 second
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("Filter");
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        ssc.sparkContext().setLogLevel("ERROR");
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+
+        //Create rdd
+        JavaRDD<Integer> rdd = ssc.sparkContext().parallelize(integers);
+        Queue<JavaRDD<Integer>> queue = new LinkedList<>();
+        queue.add(rdd);
+        //Create dstream from a queue of rdds
+        JavaDStream<Integer> integers2 = ssc.queueStream(queue);
+
+        // Regular Map = Takes one element and produces one element
+        JavaDStream<Integer> doubleIntegers = integers2.filter(new Function<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer value) throws Exception {
+                return value != 3;
+            }
+        });
+        doubleIntegers.print();
+        // 1, 2, 4
+
+        ssc.start();              // Start the computation
+        ssc.awaitTermination();   // Wait for the computation to terminate
+
+    }
+}
---
+++ teachingmaterials/src/main/java/de/dfki/streamline/hackathon/spark/FlatMap.java
@@ -0,0 +1,46 @@
+package de.dfki.streamline.hackathon.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import java.util.*;
+
+/**
+ * @author behrouz
+ */
+public class FlatMap {
+    public static void main(String[] args) throws InterruptedException {
+        // Create a local StreamingContext with two working thread and batch interval of 1 second
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("FlatMap");
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        ssc.sparkContext().setLogLevel("ERROR");
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+
+        //Create rdd
+        JavaRDD<Integer> rdd = ssc.sparkContext().parallelize(integers);
+        Queue<JavaRDD<Integer>> queue = new LinkedList<>();
+        queue.add(rdd);
+        //Create dstream from a queue of rdds
+        JavaDStream<Integer> integers2 = ssc.queueStream(queue);
+
+        // Regular Map = Takes one element and produces one element
+        JavaDStream<Integer> doubleIntegers = integers2.flatMap(new FlatMapFunction<Integer, Integer>() {
+            @Override
+            public Iterator<Integer> call(Integer value) throws Exception {
+                return Arrays.asList(value * 2).iterator();
+            }
+        });
+
+        doubleIntegers.print();
+        // 2, 4, 6, 8
+
+        ssc.start();              // Start the computation
+        ssc.awaitTermination();   // Wait for the computation to terminate
+
+    }
+}
